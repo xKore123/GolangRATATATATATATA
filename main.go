07ad4250ac59ed580082020849178329dbb4edb9
Muhammad Hisham Tarsoo <70771759+htarsoo@users.noreply.github.com>
2023-01-13 08:46:49 +0400

    Update main.go

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates := bot.GetUpdatesChan(u)
+	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil {
